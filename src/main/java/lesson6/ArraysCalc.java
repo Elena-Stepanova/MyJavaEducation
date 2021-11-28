5da790e8aebb4be3f599147b05bda2a638d2180f
Elena-Stepanova <93592820+Elena-Stepanova@users.noreply.github.com>
2021-11-28 16:02:23 +0600

    дз6 (1) Поиск min, max и ср.арифм. значений в массиве

---
+++ src/main/java/lesson6/ArraysCalc.java
@@ -0,0 +1,56 @@
+package lesson6;
+
+//Поиск минимального, максимального и среднего арифметического в массиве
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraysCalc {
+    public static void main(String[] args) {
+        System.out.println("Укажите длину массива  ");
+        Scanner console = new Scanner(System.in);
+
+        while (!console.hasNextInt()) {
+            console.next();
+            System.out.println("Попробуйте еще раз ");
+        }
+        int num = console.nextInt();
+        System.out.println("Создан массив из " + num + " чисел");
+        int [] numeric = new int[num];
+
+        for (int i = 0; i < numeric.length; i++){
+        numeric[i] = (int)(Math.random() * 30);
+
+        }
+        System.out.println(Arrays.toString(numeric));
+        System.out.println("Минимальное значение в массиве равно " + minimum(numeric));
+        System.out.println("Максимальное значение в массиве равно " + maximum(numeric));
+        System.out.println("Среднее арифметическое всех значений в массиве равно " + average(numeric));
+    }
+    public static int minimum(int [] numeric){
+        int minValue = numeric[0];
+        for (int i = 1; i < numeric.length; i++){
+            if (numeric[i] < minValue)
+                minValue = numeric[i];
+        }
+        return (minValue);
+    }
+
+    public static int maximum(int [] numeric){
+        int maxValue = numeric[0];
+        for (int i = 1; i < numeric.length; i++){
+            if (numeric[i] > maxValue)
+                maxValue = numeric[i];
+        }
+        return (maxValue);
+    }
+
+    public static double average(int [] numeric){
+        int sum = numeric[0];
+        double aver = 0.0;
+        for (int i = 1; i < numeric.length; i++){
+            sum = (numeric[i] + sum) ;
+        }
+        return (aver = sum * 1.0 / numeric.length); //можно еще округлить до 1-2 десятичных знаков
+    }
+}
