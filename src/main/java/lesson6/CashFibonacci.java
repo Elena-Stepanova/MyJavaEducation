310d4c2a1f8f83785ba61deec7914a22e508d47e
Степанова Елена Михайловна <stemix74@gmail.com>
2021-11-30 15:05:13 +0600

    дз 6 (2) окончательно запуталась

--- src/main/java/lesson6/CashFibonacci.java
+++ src/main/java/lesson6/CashFibonacci.java
@@ -7,7 +7,7 @@ public class CashFibonacci {
     static int limit = 20;
     public static void main(String[] args) {
 
-        System.out.printf("Введите положительное число не более %d\n", limit);
+        System.out.printf("Введите позицию в ряде Фибоначчи как целое положительное число не более %d\n", limit);
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -19,15 +19,15 @@ public class CashFibonacci {
                     System.out.printf("Введено отрицательное число или число больше %d. Попробуйте снова\n", limit);
                     continue;
                 }
-            } else if (scanner.hasNextLine() && scanner.nextLine().equals("stop")) {
+            } else if (scanner.hasNextLine() && scanner.nextLine().equalsIgnoreCase("stop")) {
                 System.out.println("Программа завершена");
                 break;
             } else {
                 System.out.println("Ошибка при обработке ввода. Попробуйте снова");
                 continue;
             }
-        }
 
+        }
     }
 
     public static int numberFib(int n) {
@@ -40,19 +40,20 @@ public class CashFibonacci {
 
     }
 
-    public static int[] fibgen() {
-        int[] accumulator = new int[limit];
+    public static int[] fibGen(int[] fibonacciAccum, int numberFib) {
+        int[] fibonacciAccum = new int[limit];
         int s = 0;
         int fib = numberFib(s);
         while (fib < limit) {
-            accumulator[s] = fib;
-            if ((accumulator[0] == 0) || (accumulator[s] > 0)){
+            fibonacciAccum[s] = fib;
+            if ((fibonacciAccum[0] == 0) || (fibonacciAccum[s] > 0)){
+                System.out.printf("Значение с индексом %d было расчитано и размещено в кэше\n", s);
                 continue;
             }
             s += 1;
             fib = numberFib(s);
         }
-        return accumulator;
+        return fibonacciAccum;
     }
 
 }
