121d2b02a7d92a5a2d6cfc131bec011f19e90887
Степанова Елена Михайловна <stemix74@gmail.com>
2021-12-02 14:10:11 +0600

    дз 6 (2) Внесены исправления, но массив работает некорректно. А также проверка на введение текста в консоль

--- src/main/java/lesson6/CashFibonacci.java
+++ src/main/java/lesson6/CashFibonacci.java
@@ -4,22 +4,33 @@ package lesson6;
 import java.util.Scanner;
 
 public class CashFibonacci {
-    static int limit = 20;
-    public static void main(String[] args) {
+    static int limit = 40;
 
-        System.out.printf("Введите позицию в ряде Фибоначчи как целое положительное число не более %d\n", limit);
+    public static void main(String[] args) {
+        int topLimit = limit + 1; //увеличиваем лимит на 1, чтобы  член последовательности, соответствующий лимиту тоже
+                                  //рассчитывался, т.к. счет идет с нуля, а не с 1
+        int[] fibonacciAccum = new int[topLimit]; //инициализируем массив для кэша
+        System.out.printf("Введите позицию в ряде Фибоначчи как целое положительное число не более %d \n", limit);
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             if (scanner.hasNextInt()) {
                 int n = scanner.nextInt();
-                if (n >= 0 && n <= limit) {
-                    System.out.println(n + "-й член последовательности Фибоначчи равен  " + numberFib(n));
+                if (n >= 0 && n <= topLimit) {
+                    if (fibonacciAccum[n] != 0) { //тут сначала смотрим в нашем кэше отличное от нуля значение, вдруг мы его посчитали и сохранили ранее
+                        System.out.println(n + "-й член последовательности Фибоначчи"  + fibonacciAccum[n]); //Тут сразу достаем это значение из кэша и выводим на консоль, ничего не считая
+
+                    } else { //сюда попадаем, если в кэше нашего значения ещё нет, и нам надо его посчитать
+                        int value = numberFib(n, fibonacciAccum); //тут вызываем метод нахождения числа и заодно передаем в метод наш массив,
+                                                                // чтобы пока будем считать сразу сохранить в него всё, что посчитаетсяя
+                        System.out.println(n + "-й член последовательности Фибоначчи был рассчитан ранее и помещён " +
+                                "в кэш.\n" + "Он равен  " + value); //выводим на консоль найденное значение
+                    }
                 } else {
                     System.out.printf("Введено отрицательное число или число больше %d. Попробуйте снова\n", limit);
                     continue;
                 }
-            } else if (scanner.hasNextLine() && scanner.nextLine().equalsIgnoreCase("stop")) {
+            } else if (scanner.nextLine().equalsIgnoreCase("stop")) {
                 System.out.println("Программа завершена");
                 break;
             } else {
@@ -30,31 +41,17 @@ public class CashFibonacci {
         }
     }
 
-    public static int numberFib(int n) {
+    public static int numberFib(int n, int[] fibonacciAccum) {
 
         if (n == 0 || n == 1) {
+            fibonacciAccum[n] = n; //тут записываем в кэш значения
             return n;
         } else {
-            return (numberFib(n - 1) + numberFib(n - 2));
+            int fibValue = (numberFib(n - 1, fibonacciAccum) + numberFib(n - 2, fibonacciAccum)); //находим значение из последовательности
+            fibonacciAccum[n] = fibValue; //когда нашли его, то записываем в кэш под тем номером, под каким оно находится в последовательности
+            return fibValue;
         }
 
     }
-/*
-    public static int[] fibGen(int[] fibonacciAccum, int numberFib) {
-        int[] fibonacciAccum = new int[limit];
-        int s = 0;
-        int fib = numberFib(s);
-        while (fib < limit) {
-            fibonacciAccum[s] = fib;
-            if ((fibonacciAccum[0] == 0) || (fibonacciAccum[s] > 0)){
-                System.out.printf("Значение с индексом %d было расчитано и размещено в кэше\n", s);
-                continue;
-            }
-            s += 1;
-            fib = numberFib(s);
-        }
-        return fibonacciAccum;
-    }
-*/
 }
 
